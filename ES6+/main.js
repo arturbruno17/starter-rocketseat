0a858e5a88eb56eab011a9da695070026557bf08
Artur Bruno <arturjuliao20@gmail.com>
2020-07-28 11:35:42 -0300

    class: Arrow Function

--- ES6+/main.js
+++ ES6+/main.js
@@ -1,29 +1,10 @@
-const arr = [1, 3, 4, 5, 8, 9]
+const arr = [1, 3, 4, 5, 6]
 
-// .map
-const newArr = arr.map(function(item, index) {
-    return item * index
-})
+const newArr = arr.map(item => item * 2)
 
 console.log(newArr)
 
-// .reduce
-const sum = arr.reduce(function(total, next) {
-    return total + next
-})
 
-console.log(sum)
+const teste = () => ({ nome: 'Diego' })
 
-// .filter
-const filter = arr.filter(function(item) {
-    return item % 2 === 0
-})
-
-console.log(filter)
-
-// .find
-const find = arr.find(function(item) {
-    return item === 4
-})
-
-console.log(find)
+console.log(teste())

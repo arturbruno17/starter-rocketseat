78baae18636dd727ab9268037f3d14b101d89b68
Artur Bruno <arturjuliao20@gmail.com>
2020-07-28 12:04:49 -0300

    class: Template Literals

--- ES6+/main.js
+++ ES6+/main.js
@@ -1,40 +1,5 @@
-// REST
+const nome = 'Artur'
+const idade = 15
 
-const usuario = {
-    nome: 'Artur',
-    idade: 15,
-    empresa: 'Rocketseat'
-}
-const { nome, ...resto } = usuario
-console.log(nome)
-console.log(resto)
-
-
-const arr = [1, 2, 3, 4]
-const [ a, b, ...c ] = arr
-console.log(a)
-console.log(b)
-console.log(c)
-
-
-function soma(...params) {
-    return params.reduce((total, next) => total + next)
-}
-console.log(soma(1, 5, 2, 4, 6, 8, 9))
-
-
-// SPREAD
-
-const arr1 = [1, 2, 3]
-const arr2 = [4, 5, 6]
-const arr3 = [...arr1, ...arr2]
-console.log(arr3);
-
-
-const usuario1 = {
-    nome: 'Artur',
-    idade: 15,
-    empresa: 'Rocketseat'
-}
-const usuario2 = {...usuario1, nome: 'Gabriel'}
-console.log(usuario2)
+// console.log('Meu nome é', nome, 'e tenho', idade, 'anos')
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`)

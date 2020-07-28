a8f3037cf17dc0e61c83eec3e89b444d9be442a2
Artur Bruno <arturjuliao20@gmail.com>
2020-07-28 12:01:28 -0300

    class: Operadores rest/spread

--- ES6+/main.js
+++ ES6+/main.js
@@ -1,14 +1,40 @@
+// REST
+
 const usuario = {
     nome: 'Artur',
     idade: 15,
-    endereco: {
-        cidade: 'Barreira',
-        estado: 'Ceará'
-    }
+    empresa: 'Rocketseat'
 }
+const { nome, ...resto } = usuario
+console.log(nome)
+console.log(resto)
 
-function mostraNome({ nome, idade }) {
-    console.log(nome, idade)
+
+const arr = [1, 2, 3, 4]
+const [ a, b, ...c ] = arr
+console.log(a)
+console.log(b)
+console.log(c)
+
+
+function soma(...params) {
+    return params.reduce((total, next) => total + next)
 }
+console.log(soma(1, 5, 2, 4, 6, 8, 9))
 
-mostraNome(usuario)
+
+// SPREAD
+
+const arr1 = [1, 2, 3]
+const arr2 = [4, 5, 6]
+const arr3 = [...arr1, ...arr2]
+console.log(arr3);
+
+
+const usuario1 = {
+    nome: 'Artur',
+    idade: 15,
+    empresa: 'Rocketseat'
+}
+const usuario2 = {...usuario1, nome: 'Gabriel'}
+console.log(usuario2)

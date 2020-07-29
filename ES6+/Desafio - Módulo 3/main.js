1ded3ce74bb071c8b105538945a22dded8953255
Artur Bruno <arturjuliao20@gmail.com>
2020-07-29 14:33:58 -0300

    add Desafio - Módulo 3

---
+++ ES6+/Desafio - Módulo 3/main.js
@@ -0,0 +1,58 @@
+// Funão delay aciona o .then após 1s
+const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
+
+async function umPorSegundo() {
+    await delay()
+    console.log('1s')
+
+    await delay()
+    console.log('2s')
+
+    await delay()
+    console.log('3s')
+}
+
+umPorSegundo();
+
+
+// ---------------------------------------------------------------
+import axios from 'axios';
+
+async function getUserFromGithub(user) {
+    try {
+        const response = await axios.get(`https://api.github.com/users/${user}`)
+        console.log(response.data)
+    } catch (err) {
+        console.log('Usuário não existe');
+    }
+}
+
+getUserFromGithub('diego3g');
+
+
+// ---------------------------------------------------------------
+class Github {
+    static async getRepositories(repo) {
+        try {
+            const response = await axios.get(`https://api.github.com/repos/${repo}`)
+            console.log(response.data)
+        } catch (err) {
+            console.log('Repositório não existe');
+        }
+    }
+}
+
+Github.getRepositories('arturbruno17/arturbruno17');
+
+
+// ---------------------------------------------------------------
+const buscaUsuario = async usuario => {
+    try {
+        const response = await axios.get(`https://api.github.com/users/${user}`)
+        console.log(response.data)
+    } catch (err) {
+            console.log('Usuário não existe');
+    };
+}
+
+buscaUsuario('diego3g');
